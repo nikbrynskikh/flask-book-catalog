ba520e8a6ab5fa41145b988915f4358ca5ca0034
nikbrynskikh <nikphoto2010@mail.ru>
2019-10-28 21:24:09 +0500

    made correction

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,6 @@
+import os
+
 debug=False
 SECRET_KEY='topsecret'
-SQLALCHEMY_DATABASE_URI='postgresql://postgres:topsecret@localhost:5432/catalog_db'
+SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL']
 SQLACHEMY_TRACK_MODIFICATION=False
